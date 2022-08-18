5c142ed7741400b930d0f9997eade7932d462b5b
Betuelferreira <betobetuelferreira@gmail.com>
2022-08-18 17:06:48 -0300

    edição

--- jogo carro/jav.js
+++ jogo carro/jav.js
@@ -2,6 +2,10 @@ const carro = document.querySelector('.carro');
 
 const jump = () => {
     carro.classList.add('jump');
+
+setTimeout ( () => {
+    carro.classList.remove('jump');
+}, 1000);
 }
 
 document.addEventListener('keydown', jump)
