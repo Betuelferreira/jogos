6eb37e2fb37ba915549729803317a4b52c0e38d2
Betuelferreira <betobetuelferreira@gmail.com>
2022-08-20 20:50:50 -0300

    ajuste4

--- jogo carro/jav.js
+++ jogo carro/jav.js
@@ -7,20 +7,28 @@ const jump = () => {
 setTimeout ( () => {
     carro.classList.remove('jump');
 }, 1000);
-
 }
 
-
-
 const loop = setInterval(() => {
+
     const posipedra = pedra.offsetLeft;
+ /* const posinuvem = nuvem1.offsetLeft; */
     const posicarro = +window.getComputedStyle(carro).bottom.replace('px',' ');
+ 
+
     if( posipedra <= 129 && posipedra > 0 && posicarro < 40  ){
+
         pedra.style.animation = 'none'
         pedra.style.left = `${posipedra}px` ;
+
+        carro.style.animation = 'none'
+        carro.style.bottom = `${posicarro}px` ; 
+
+        /* nuvem1.style.animation = 'none'
+        nuvem1.style.left = `${posinuvem}px` ; */ 
+        
+        carro.src = './imagens/carrob.png'
     }
-
-
 }, 10);
 
 document.addEventListener('keydown', jump)
