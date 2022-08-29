05a4c760b97aa84d84e3d92f0597e3fc585c69f7
Betuelferreira <111235101+Betuelferreira@users.noreply.github.com>
2022-08-29 16:41:10 -0300

    Update jav.js

--- jogo carro/jav.js
+++ jogo carro/jav.js
@@ -56,6 +56,5 @@ const loop = setInterval(() => {
  
 }, 10);
 
-/*document.addEventListener('keydown', jump)*/
-document.addEventListener('keyup', jump)
+document.addEventListener('keydown', jump)
 
