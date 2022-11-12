44048d5441b3c7045570a85af45057dac0160316
Betuelferreira <betobetuelferreira@gmail.com>
2022-11-12 12:24:45 -0300

    mudar

--- jogo carro/jav.js
+++ jogo carro/jav.js
@@ -1,7 +1,7 @@
 const carro = document.querySelector('.carro');
 const pedra = document.querySelector('.pedra');
 const perdeu = document.getElementById('perdeu');
-const pontos = document.getElementById('pontos'); 
+const pontos = document.getElementById('pontos');
 const botao = document.getElementById('botao');
 const botao2 = document.getElementById('botao2');
 
@@ -17,75 +17,86 @@ let playerPontos = 0;
 
 
 
-const inicio = () =>{
+const inicio = () => {
     pedra.classList.add('anpedra');
     pedra.style.display = "unset"
     carro.style.display = "unset"
     botao.style.display = "none"
     game = true;
-} 
+}
 botao.addEventListener('click', inicio)
 
-/* const reiniciar = () =>{
+/* const reiniciar = () => {
     pedra.classList.add('anpedra');
     pedra.style.display = "unset"
     carro.style.display = "unset"
-    
+
+
     botao2.style.display = "none"
     perdeu.style.display = "none"
 
     game = true;
-} 
-botao2.addEventListener('click', reiniciar) */
-
+}
+botao2.addEventListener('click', reiniciar)
+ */
 
 let contPont = () => {
-    if (game){
-    playerPontos++;
-    pontos.innerHTML = `PONTOS: <b>${playerPontos}</b>`;}
-    
+    if (game) {
+        playerPontos++;
+        pontos.innerHTML = `PONTOS: <b>${playerPontos}</b>`;
+    }
+
 }
-interval = setInterval(contPont,200);
+interval = setInterval(contPont, 200);
 
 
 const jump = () => {
     carro.classList.add('jump');
 
-setTimeout ( () => {
-    carro.classList.remove('jump');
-}, 1000);
+    setTimeout(() => {
+        carro.classList.remove('jump');
+    }, 1000);
 }
 
 
 
 const loop = setInterval(() => {
 
-    const posipedra = pedra.offsetLeft;
- /* const posinuvem = nuvem1.offsetLeft; */
-    const posicarro = +window.getComputedStyle(carro).bottom.replace('px',' ');
-    
 
-    if( posipedra <= 129 && posipedra > 0 && posicarro < 40  ){
+    const posipedra = pedra.offsetLeft;
+    /* const posinuvem = nuvem1.offsetLeft; */
+    const posicarro = +window.getComputedStyle(carro).bottom.replace('px', ' ');
+
+
+    if (posipedra <= 129 && posipedra > 0 && posicarro < 40) {
 
         pedra.style.animation = 'none'
-        pedra.style.left = `${posipedra}px` ;
+        pedra.style.left = `${posipedra}px`;
 
         carro.style.animation = 'none'
-        carro.style.bottom = `${posicarro}px` ; 
-        
-        carro.src = './imagens/carrob.png'
-        
+        carro.style.bottom = `${posicarro}px`;
 
-        perdeu.style.display ='unset'
-       
+
+        /* carro.src = './imagens/carrob.png' */
+
+
+        perdeu.style.display = 'unset'
+
+
         game = false
 
-        /* botao2.style.display ='unset' */
+
+
+        /* botao2.style.display = 'unset' */
+
+
 
 
     }
- 
+
+
+
+
 }, 10);
 
 document.addEventListener('keydown', jump)
-
